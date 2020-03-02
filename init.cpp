010f1653ff16cbcdc35df1d96ca2762cf3909e9b
Nathan Kulzer <nathan0kulzer@gmail.com>
2020-03-02 09:05:04 -0500

    Write code

--- init.cpp
+++ init.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+int main(int argc, char** argv) {
+	std::cout << "Program name";
+	if (argc >0) {
+		std::cout << ": " << argv[0];
+	} else {
+		std::cout << "less";
+	}
+	std::cout << "\n";
+	for (int i = 1; i < argc; i++) {
+		
+	}
+}
